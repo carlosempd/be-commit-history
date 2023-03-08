a6e9d6332d33f3f07cbf29df0816c4e82f338d7a
Carlos <carlosempd@gmail.com>
2023-03-08 11:21:51 -0400

    implement commits service and connect with github api to retrieve the commits list

--- src/commits/commits.service.ts
+++ src/commits/commits.service.ts
@@ -1,4 +1,27 @@
 import { Injectable } from '@nestjs/common';
+import { Octokit } from 'octokit';
+import { Commit } from 'src/core/models/commit.model';
+
+const octokit = new Octokit({});
 
 @Injectable()
-export class CommitsService {}
+export class CommitsService {
+
+    async findAll(repo: 'be' | 'fe'): Promise<Commit[]> {
+        const response = await octokit.request(
+            `Get /repos/carlosempd/${ repo }-commit-history/commits`,
+        );
+
+        return response.data.map(el => ({
+            sha: el.sha,
+            message: el.commit.message,
+            date: el.commit.author.date,
+            author: {
+                login: el.author.login,
+                name: el.commit.author.name,
+                email: el.commit.author.email,
+                avatar: el.author.avatar_url
+            }
+        }));
+    }
+}
