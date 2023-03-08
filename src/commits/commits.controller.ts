c04e209b3796232ed113c34e21c73e42fee88974
Carlos <carlosempd@gmail.com>
2023-03-08 11:22:21 -0400

    update controller with service and add types

--- src/commits/commits.controller.ts
+++ src/commits/commits.controller.ts
@@ -1,26 +1,15 @@
-import { Controller, Get } from '@nestjs/common';
-import { Octokit } from 'octokit';
+import { Controller, Get, Param, Query, Req } from '@nestjs/common';
+import { CommitsService } from './commits.service';
+import { Commit } from 'src/core/models/commit.model';
 
-const octokit = new Octokit({});
 
 @Controller('commits')
 export class CommitsController {
-    @Get()
-    async findAll() {
-        const response = await octokit.request(
-            'Get /repos/carlosempd/be-commit-history/commits',
-        );
 
-        return response.data.map(el => ({
-            sha: el.sha,
-            message: el.commit.message,
-            date: el.commit.author.date,
-            author: {
-                login: el.author.login,
-                name: el.commit.author.name,
-                email: el.commit.author.email,
-                avatar: el.author.avatar_url
-            }
-        }));
+    constructor(private commitService: CommitsService) {}
+
+    @Get()
+    findAll(@Query('repo') repo: 'be' | 'fe'): Promise<Commit[]> {
+        return this.commitService.findAll(repo);
     }
 }
